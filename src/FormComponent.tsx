03ca562a93d8762d719ba9307cb2f792f9a2cb7c
Jair José Suárez López <jjsuarez8@hotmail.es>
2021-09-23 11:52:37 -0500

    Use Formik componets

--- src/FormComponent.tsx
+++ src/FormComponent.tsx
@@ -1,49 +1,50 @@
 // import { useForm } from "./useForm";
 
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { object, string } from "yup";
 
+import "./styles.scss";
+
 export function FormComponent() {
-  const form = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: ""
-    },
-    validationSchema: object({
-      firstName: string()
-        .max(15, "Must be 15 characters or less")
-        .required("This field is required"),
-      lastName: string()
-        .max(20, "Must be 20 characters or less")
-        .required("This field is required"),
-      email: string()
-        .email("Invalid email address")
-        .required("This field is required")
-    }),
-    onSubmit: (values) => {
-      console.log("onSubmit", values);
-    }
-  });
-
-  console.log(form);
-  const { handleSubmit, getFieldProps, errors, touched } = form;
-
   return (
-    <form noValidate onSubmit={handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input type="text" {...getFieldProps("firstName")} />
-      {errors.firstName && touched.firstName && <div>{errors.firstName}</div>}
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: ""
+      }}
+      validationSchema={object({
+        firstName: string()
+          .max(15, "Must be 15 characters or less")
+          .required("This field is required"),
+        lastName: string()
+          .max(20, "Must be 20 characters or less")
+          .required("This field is required"),
+        email: string()
+          .email("Invalid email address")
+          .required("This field is required")
+      })}
+      onSubmit={(data) => {
+        setTimeout(() => {
+          console.log(data);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field type="text" name="firstName" />
+        <ErrorMessage name="firstName" />
 
-      <label htmlFor="lastName">Last Name</label>
-      <input type="text" {...getFieldProps("lastName")} />
-      {errors.lastName && touched.lastName && <div>{errors.lastName}</div>}
+        <label htmlFor="lastName">Last Name</label>
+        <Field type="text" name="lastName" />
+        <ErrorMessage name="lastName" />
 
-      <label htmlFor="email">Email Address</label>
-      <input type="email" {...getFieldProps("email")} />
-      {errors.email && touched.email && <div>{errors.email}</div>}
+        <label htmlFor="email">Email Address</label>
+        <Field type="email" name="email" />
+        <ErrorMessage name="email" />
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 }
